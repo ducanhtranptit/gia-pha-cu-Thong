3afaa66a50bda1bc3288dec201e3fdadf9ce5efd
ducanhtranptit <tda.ducanh@gmail.com>
2024-05-04 13:35:57 +0700

    update folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TreeComponent from "./components/TreeComponent";
+
+function App() {
+	return (
+		<div className="App">
+			<TreeComponent />
+		</div>
+	);
+}
+
+export default App;
