113caca9920b65b34818f8217576cabcd68c5e33
ducanhtranptit <tda.ducanh@gmail.com>
2024-05-26 07:32:40 +0700

    update baseurl for backend

--- backend/app/apis/people.api.js
+++ backend/app/apis/people.api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const PeopleController = require("../../controllers/people.controller");
+const PeopleController = require("../controllers/people.controller");
 
 router.get("/", PeopleController.getAllMalePeopleByFilter)
 router.get("/family-tree", PeopleController.getAllPeople);
