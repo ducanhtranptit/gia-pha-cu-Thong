3c93536dad075619686110ab74afc3843981cd12
nguyenducthanh04 <dducthanh04@gmail.com>
2024-05-27 22:46:16 +0700

    update ui familytree

--- frontend/web-app/src/App.jsx
+++ frontend/web-app/src/App.jsx
@@ -11,7 +11,6 @@ import "./App.css";
 const App = () => (
     <Router>
         <div className="app">
-            <h1>Test git</h1>
             <Navbar />
             <div className="content">
                 <Routes>
--- frontend/web-app/src/config/config.js
+++ frontend/web-app/src/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    baseUrl: "http://localhost:2504/api/v1/core"
-}
+    baseUrl: "http://localhost:3002/api/v1/core",
+};
