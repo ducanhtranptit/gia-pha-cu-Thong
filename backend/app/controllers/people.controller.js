49ffb769889b5e9aadc12ba39923861945aa1308
ducanhtranptit <tda.ducanh@gmail.com>
2024-06-24 13:52:59 +0700

    update create person modal

--- backend/app/controllers/people.controller.js
+++ backend/app/controllers/people.controller.js
@@ -1,105 +1,95 @@
 const PeopleActions = require("../actions/people.actions");
-const {
-    SuccessResponse,
-    ErrorResponse,
-    ForbiddenResponse,
-} = require("../core/ApiResponse.js");
+const { SuccessResponse, ErrorResponse, ForbiddenResponse } = require("../core/ApiResponse.js");
 
 class PeopleController {
-    async getAllPeople(req, res) {
-        try {
-            const people = await PeopleActions.getAllPeopleOfFamilyTree();
-            return new SuccessResponse().send(req, res, people);
-        } catch (error) {
-            console.error(error);
-            return new ErrorResponse().send(req, res);
-        }
-    }
+	async getAllPeople(req, res) {
+		try {
+			const people = await PeopleActions.getAllPeopleOfFamilyTree();
+			return new SuccessResponse().send(req, res, people);
+		} catch (error) {
+			console.error(error);
+			return new ErrorResponse().send(req, res);
+		}
+	}
 
-    async getAllMalePeopleByFilter(req, res) {
-        try {
-            const filters = req.query;
-            const malePeople = await PeopleActions.getAllPeople(filters);
-            return new SuccessResponse().send(req, res, malePeople);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
+	async getAllMalePeopleByFilter(req, res) {
+		try {
+			const filters = req.query;
+			const malePeople = await PeopleActions.getAllPeople(filters);
+			return new SuccessResponse().send(req, res, malePeople);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
 
-    async createPerson(req, res) {
-        try {
-            const { person, spouse } = req.body;
-            if (!person) {
-                return new ForbiddenResponse().send(req, res);
-            }
-            const newPerson = await PeopleActions.createPersonAndTheirSpouse(
-                person,
-                spouse
-            );
-            return new SuccessResponse().send(req, res, newPerson);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
+	async createPerson(req, res) {
+		try {
+			const { person, spouse } = req.body;
+			if (!person) {
+				return new ForbiddenResponse().send(req, res);
+			}
+			const newPerson = await PeopleActions.createPersonAndTheirSpouse(person, spouse);
+			return new SuccessResponse().send(req, res, newPerson);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
 
-    async getPersonDetails(req, res) {
-        try {
-            const filters = req.query;
-            const person = await PeopleActions.findOneForProfilePage(filters);
-            return new SuccessResponse().send(req, res, person);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
+	async getPersonDetails(req, res) {
+		try {
+			const filters = req.query;
+			const person = await PeopleActions.findOneForProfilePage(filters);
+			return new SuccessResponse().send(req, res, person);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
 
-    async getAllPeopleOfManager(req, res) {
-        try {
-            const people = await PeopleActions.getAllPeopleOfManagerByFilters(req.query);
-            return new SuccessResponse().send(req, res, people);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
-    async getAllFather(req, res) {
-        try {
-            const fathers = await PeopleActions.getAllFather();
-            return new SuccessResponse().send(req, res, fathers);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
-    async deletePerson(req, res) {
-        try {
-            const { id } = req.params;
-            await PeopleActions.deletePerson(id);
-            return new SuccessResponse().send(req, res);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
-    async updateDataPerson(req, res) {
-        try {
-            const { id } = req.params;
-            const dataPerson = req.body;
-            if (!dataPerson) {
-                return new ForbiddenResponse().send(req, res);
-            }
-            const updatedPerson = await PeopleActions.updateDataPerson(
-                id,
-                dataPerson
-            );
-            return new SuccessResponse().send(req, res, updatedPerson);
-        } catch (error) {
-            console.error(error.message);
-            return new ErrorResponse().send(req, res);
-        }
-    }
+	async getAllPeopleOfManager(req, res) {
+		try {
+			const people = await PeopleActions.getAllPeopleOfManagerByFilters(req.query);
+			return new SuccessResponse().send(req, res, people);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
+	async getAllFather(req, res) {
+		try {
+			const fathers = await PeopleActions.getAllFather();
+			return new SuccessResponse().send(req, res, fathers);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
+	async deletePerson(req, res) {
+		try {
+			const { id } = req.params;
+			await PeopleActions.deletePerson(id);
+			return new SuccessResponse().send(req, res);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
+	async updateDataPerson(req, res) {
+		try {
+			const { id } = req.params;
+			const dataPerson = req.body;
+			if (!dataPerson) {
+				return new ForbiddenResponse().send(req, res);
+			}
+			const updatedPerson = await PeopleActions.updateDataPerson(id, dataPerson);
+			return new SuccessResponse().send(req, res, updatedPerson);
+		} catch (error) {
+			console.error(error.message);
+			return new ErrorResponse().send(req, res);
+		}
+	}
 }
 
 module.exports = new PeopleController();
