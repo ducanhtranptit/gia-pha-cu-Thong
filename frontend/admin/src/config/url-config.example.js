219c105d226cfa84b2c4d10492368345a6d3a769
ducanhtranptit <tda.ducanh@gmail.com>
2024-07-28 19:00:08 +0700

    [update]: Update env example

--- frontend/admin/src/config/url-config.example.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	baseUrl: "http://localhost:2504/api/v1/core",
-};
--- frontend/web-app/src/config/url-config.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	baseUrl: "http://localhost:2504/api/v1/core",
-    adminUrl: "http://localhost:3311"
-};
